1e931cb0d2e1317a242cae16a72cb6aaebbbdc5c
Dave Niewinski <davesarmoury@gmail.com>
2021-06-18 13:49:40 -0400

    Added gripper controller and bringup

--- operation_control/scripts/frame_transformer.py
+++ operation_control/scripts/frame_transformer.py
@@ -12,6 +12,8 @@ Y_MIN=0.0
 Y_MAX=0.5
 Z_MIN=0.0
 Z_MAZ=0.3
+TILT_MIN=-1.0
+TILT_MAX=1.0
 
 tool_mtx = reset_frame()
 
@@ -52,12 +54,19 @@ def update_frame_callback(msg):
         shift = compose_matrix(angles=(0,0,rot))
     elif msg.data.upper() == "RZ-":
         shift = compose_matrix(angles=(0,0,-rot))
+    elif msg.data.upper() == "RET":
+        shift = compose_matrix(translate=(0,0,0.1))
     else:
         rospy.logwarn("Invalid command " + str(msg.data))
         return
 
     tmp_mtx = numpy.matmul(tool_mtx, shift)
-    tool_mtx = tmp_mtx
+    ####################
+    scale, shear, angles, translate, perspective = decompose_matrix(tmp_mtx)
+    translate_limit = [max(min(translate[0],X_MAX),X_MIN), max(min(translate[1],Y_MAX),Y_MIN), max(min(translate[2],Z_MAX),Z_MIN)]
+    angles_limit = [max(min(angles[0],TILT_MAX),TILT_MIN), max(min(angles[1],TILT_MAX),TILT_MIN), angles[2]]
+    ######################
+    tool_mtx = compose_matrix(translate=translate_limit, angles=angles_limit)
 
 if __name__ == '__main__':
     global tool_mtx
@@ -65,7 +74,7 @@ if __name__ == '__main__':
     br = tf.TransformBroadcaster()
     tool_mtx = reset_frame()
 
-    rospy.Subscriber("frame_action", String, update_frame_callback)
+    rospy.Subscriber("vote_result", String, update_frame_callback)
 
     r = rospy.Rate(5) # 5hz
     while not rospy.is_shutdown():
---
+++ operation_control/scripts/gripper_controller.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from robotiq_2f_gripper_control.msg import Robotiq2FGripper_robot_output
+
+OPEN = 128
+CLOSE = 255
+SPEED = 10
+FORCE = 5
+
+def halfOpenMsg():
+    msg = openMsg()
+    msg.rPR = int((OPEN + CLOSE)/2)
+    return msg
+
+def closeMsg():
+    msg = openMsg()
+    msg.rPR = CLOSE
+    return msg
+
+def openMsg():
+    msg = activateMsg()
+    msg.rPR = OPEN
+    msg.rSP = SPEED
+    msg.rFR = FORCE
+    return msg
+
+def activateMsg():
+    msg = Robotiq2FGripper_robot_output()
+    msg.rACT = 1
+    msg.rGTO = 1
+    msg.rATR = 0
+    msg.rPR = 0
+    msg.rSP = 255
+    msg.rFR = 150
+    return msg
+
+def resetMsg():
+    msg = Robotiq2FGripper_robot_output()
+    msg.rACT = 0
+    msg.rGTO = 0
+    msg.rATR = 0
+    msg.rPR = 0
+    msg.rSP = 0
+    msg.rFR = 0
+    return msg
+
+def update_gripper_callback(msg):
+    global pub
+    if msg.data.upper() == "RET":
+        pub.publish(resetMsg())
+        pub.publish(halfOpenMsg())
+    if msg.data.upper() == "GO":
+        pub.publish(resetMsg())
+        pub.publish(openMsg())
+    if msg.data.upper() == "GC":
+        pub.publish(resetMsg())
+        pub.publish(closeMsg())
+    if msg.data.upper() == "GH":
+        pub.publish(resetMsg())
+        pub.publish(halfOpenMsg())
+
+if __name__ == '__main__':
+    global pub
+    rospy.init_node('operation_gripper_controller')
+
+    rospy.Subscriber("vote_result", String, update_gripper_callback)
+    pub = rospy.Publisher('/Robotiq2FGripperRobotOutput', Robotiq2FGripper_robot_output, queue_size=10)
+
+    pub.publish(resetMsg())
+    rospy.sleep(1.0)
+    pub.publish(activateMsg())
+    rospy.sleep(2.0)
+
+    rospy.spin()
